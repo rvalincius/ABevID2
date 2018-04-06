3f6338377cad44ec024624dcfc94e87dd30b6762
jmoser92 <35816394+jmoser92@users.noreply.github.com>
2018-04-06 10:54:51 -0400

    Barcode scanning using Zxing added

--- app/src/main/java/com/abevid/abevid/MainActivity.java
+++ app/src/main/java/com/abevid/abevid/MainActivity.java
@@ -1,20 +1,69 @@
 package com.abevid.abevid;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-public class MainActivity extends AppCompatActivity
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
+
     }
+
+    public void onClick(View v){
+    //respond to clicks
+        if(v.getId()==R.id.scan_button){
+    //scan
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    //retrieve scan result
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (scanningResult != null) {
+        //we have a result
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+
+            //grab the result of the scanner and insert it into the upcNumber field
+            EditText editText = (EditText)findViewById(R.id.upcNumber);
+            editText.setText(scanContent,TextView.BufferType.EDITABLE);
+
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+
+
     public void buttonOnClick (View v)
     {
         Button button = (Button) v;
