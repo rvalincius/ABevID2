a19d7079cd2dfd895da8543f397dd1ba58eb3f5e
jmoser92 <35816394+jmoser92@users.noreply.github.com>
2018-04-06 16:53:07 -0400

    Linking to API

--- app/src/main/java/com/abevid/abevid/ResultsActivity.java
+++ app/src/main/java/com/abevid/abevid/ResultsActivity.java
@@ -3,11 +3,20 @@ package com.abevid.abevid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+
 public class ResultsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+    }
+
+    protected void displayResults(String upcN) throws IOException, ParseException {
+        UPCLookup input = new UPCLookup(upcN);
+        input.getMap();
     }
 }
