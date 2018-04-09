4770b63e7674f4074c0cd6446ff61726d232405c
jmoser92 <35816394+jmoser92@users.noreply.github.com>
2018-04-09 14:04:00 -0400

    Reverted last changes that created App crash

--- app/src/main/java/com/abevid/abevid/MainActivity.java
+++ app/src/main/java/com/abevid/abevid/MainActivity.java
@@ -1,6 +1,6 @@
 package com.abevid.abevid;
 
-import com.abevid.*;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,18 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     private Button scanBtn;
     private TextView formatTxt, contentTxt;
-    private String upc;
-    private EditText editText = (EditText)findViewById(R.id.upcNumber);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,48 +24,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //build scanner button
         scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+
         scanBtn.setOnClickListener(this);
 
     }
 
-    public void onClick(View v) {
-        //method to initiate click action
-        switch(v.getId()) {
-            case R.id.scan_button:
-                IntentIntegrator scanIntegrator = new IntentIntegrator(this);
-                scanIntegrator.initiateScan();
-                break;
-            case R.id.enterbutton:
-
-                Intent intent = new Intent(this, ResultsActivity.class);
-                startActivity(intent);
-                break;
+    public void onClick(View v){
+        //respond to clicks
+        if(v.getId()==R.id.scan_button){
+            //scan
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
+            scanIntegrator.initiateScan();
         }
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        //retrieve the scanner results
+        //retrieve scan result
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
 
         if (scanningResult != null) {
-            //execute if a barcode was successfully scanned
+//we have a result
             String scanContent = scanningResult.getContents();
             String scanFormat = scanningResult.getFormatName();
 
-            //grab the result of the scanner and insert it into the upcNumber field
+            EditText editText = (EditText)findViewById(R.id.upcNumber);
             editText.setText(scanContent,TextView.BufferType.EDITABLE);
-            upc=scanContent;
 
         }
-        //Inform user that no data was received from scanner
         else{
             Toast toast = Toast.makeText(getApplicationContext(),
                     "No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
         }
 
+    }
+
+
+    public void buttonOnClick (View v)
+    {
+        Button button = (Button) v;
+        Intent intent = new Intent(this, ResultsActivity.class);
+        startActivity(intent);
     }
 
 }
