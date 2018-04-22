d79b168aa29915a94141ab5464454d62c8aff62f
jmoser92 <35816394+jmoser92@users.noreply.github.com>
2018-04-22 16:13:36 -0400

    Completed results filtering via CheckBox values

--- app/src/main/java/com/abevid/abevid/MainActivity.java
+++ app/src/main/java/com/abevid/abevid/MainActivity.java
@@ -1,8 +1,6 @@
 package com.abevid.abevid;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,17 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
-
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button scanBtn, enterBtn;
-    private TextView formatTxt, contentTxt, apiResult;
+    public static CheckBox cServings,cVitA,cVitC,cFats,cCholest,cCalcium,cSodium,cIron,cCarbs,cFiber,cProtein;
+    private TextView formatTxt, contentTxt;
     protected static String scanContent;
 
     @Override
@@ -28,15 +24,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //build scanner button
-        scanBtn = (Button) findViewById(R.id.scan_button);
-        enterBtn = (Button) findViewById(R.id.enter_button);
-        formatTxt = (TextView) findViewById(R.id.scan_format);
-        contentTxt = (TextView) findViewById(R.id.scan_content);
+        //build buttons
+        scanBtn = findViewById(R.id.scan_button);
+        enterBtn = findViewById(R.id.enter_button);
+
+        //build checkboxes
+        formatTxt = findViewById(R.id.scan_format);
+        contentTxt = findViewById(R.id.scan_content);
+        cServings=findViewById(R.id.servings);
+        cVitA=findViewById(R.id.vitA);
+        cFats=findViewById(R.id.fats);
+        cVitC=findViewById(R.id.vitC);
+        cCholest=findViewById(R.id.cholest);
+        cCalcium=findViewById(R.id.calcium);
+        cSodium=findViewById(R.id.sodium);
+        cIron=findViewById(R.id.iron);
+        cCarbs=findViewById(R.id.carbs);
+        cFiber=findViewById(R.id.fiber);
+        cProtein=findViewById(R.id.protein);
 
         scanBtn.setOnClickListener(this);
         enterBtn.setOnClickListener(this);
-
 
     }
 
@@ -49,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else {
 
-                EditText scanText = (EditText) findViewById(R.id.upcNumber);
+                EditText scanText = findViewById(R.id.upcNumber);
                 scanContent = scanText.getText().toString();
                 Log.d("MainActivity.onClick ", "Barcode:"+scanContent);
             if(scanContent!= null) {
@@ -72,27 +80,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             String scanContent = scanningResult.getContents();
             String scanFormat = scanningResult.getFormatName();
 
-            EditText editText = (EditText) findViewById(R.id.upcNumber);
+            EditText editText = findViewById(R.id.upcNumber);
             editText.setText(scanContent, TextView.BufferType.EDITABLE);
-
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "No value returned by scan", Toast.LENGTH_SHORT).show();
         }
     }
-
-    public void onCheckboxClicked(View view){
-        ArrayList al = new ArrayList();
-        boolean checked = ((CheckBox) view).isChecked();
-        switch (view.getId()){
-            case R.id.calories:
-                if (checked)
-                    al.add("calories");
-                else
-                    break;
-            case R.id.serving:
-                if(checked)
-                    al.add("serving_size");
-                else
-                    break;
-        }
-    }
-
 }
--- app/src/main/java/com/abevid/abevid/ResultsActivity.java
+++ app/src/main/java/com/abevid/abevid/ResultsActivity.java
@@ -36,51 +36,83 @@ public class ResultsActivity extends AppCompatActivity {
 
     private void showData() {
 
-         /*
-          * Construct the ArrayList data source
-          * This section needs to be adjusted and streamlined
-          */
+        /*
+         * Construct the ArrayList data source
+         * This section needs to be adjusted and streamlined
+         */
+        ArrayList<Items> arrayOfItems = new ArrayList<>();
 
-         Items iBName= new Items("Brand Name",UPCLookup.values.get("brand_name"));
-         Items iName= new Items("Item Name",UPCLookup.values.get("item_name"));
-         Items iCalories= new Items("Calories",UPCLookup.values.get("nf_calories"));
-         Items iServSize= new Items("Serving Size",UPCLookup.values.get("nf_serving_size_qty")+" "+UPCLookup.values.get("nf_serving_size_unit")+" ("+UPCLookup.values.get("nf_serving_weight_grams")+"grams)");
-         Items iFatCal= new Items("Calories from fat",UPCLookup.values.get("nf_calories_from_fat"));
-         Items iTotalFat= new Items("Total fat",UPCLookup.values.get("nf_total_fat"));
-         Items iSatFat= new Items("Saturated fat",UPCLookup.values.get("nf_saturated_fat"));
-         Items iTransFat= new Items("Trans fat",UPCLookup.values.get("nf_trans_fatty_acid"));
-         Items iPolyFat= new Items("Polyunsaturated fat",UPCLookup.values.get("nf_polyunsaturated_fat"));
-         Items iCholest= new Items("Monounsaturated fat",UPCLookup.values.get("nf_monounsaturated_fat"));
-         Items iSodium= new Items("Sodium",UPCLookup.values.get("nf_sodium"));
-         Items iTotCarb= new Items("Total Carbs",UPCLookup.values.get("nf_total_carbohydrate"));
-         Items iFiber= new Items("Dietary Fiber",UPCLookup.values.get("nf_dietary_fiber"));
-         Items iSugar= new Items("Total sugars",UPCLookup.values.get("nf_sugars"));
-         Items iProtein= new Items("Total protein",UPCLookup.values.get("nf_protein"));
-         Items iVitA= new Items("DV% vitamin A",UPCLookup.values.get("nf_vitamin_a_dv"));
-         Items iVitC= new Items("DV% vitamin C",UPCLookup.values.get("nf_vitamin_c_dv"));
-         Items iCalcium= new Items("DV% calcium",UPCLookup.values.get("nf_calcium_dv"));
-         Items iIron= new Items("DV% iron",UPCLookup.values.get("nf_iron_dv"));
+        // Build default items for list
+        Items iBName= new Items("Brand Name",UPCLookup.values.get("brand_name"));
+        arrayOfItems.add(iBName);
+        Items iName= new Items("Item Name",UPCLookup.values.get("item_name"));
+        arrayOfItems.add(iName);
+        Items iCalories= new Items("Calories",UPCLookup.values.get("nf_calories"));
+        arrayOfItems.add(iCalories);
 
-         ArrayList<Items> arrayOfItems = new ArrayList<>();
-         arrayOfItems.add(iName);arrayOfItems.add(iBName);
-         arrayOfItems.add(iCalories);
-         arrayOfItems.add(iServSize);
-         arrayOfItems.add(iFatCal);
-         arrayOfItems.add(iTotalFat);
-         arrayOfItems.add(iSatFat);
-         arrayOfItems.add(iTransFat);
-         arrayOfItems.add(iPolyFat);
-         arrayOfItems.add(iCholest);
-         arrayOfItems.add(iSodium);
-         arrayOfItems.add(iTotCarb);
-         arrayOfItems.add(iFiber);
-         arrayOfItems.add(iSugar);
-         arrayOfItems.add(iProtein);
-         arrayOfItems.add(iVitA);
-         arrayOfItems.add(iVitC);
-         arrayOfItems.add(iCalcium);
-         arrayOfItems.add(iIron);
-         arrayOfItems.add(iProtein);
+
+        /*
+        *   Building items from checkboxes
+        */
+
+        //  Build ListView items for user selected values
+        if(MainActivity.cServings.isChecked()){
+            Items iServSize= new Items("Serving Size",UPCLookup.values.get("nf_serving_size_qty")+" "+UPCLookup.values.get("nf_serving_size_unit")+" ("+UPCLookup.values.get("nf_serving_weight_grams")+"grams)");
+            arrayOfItems.add(iServSize);
+        }
+        if(MainActivity.cFats.isChecked()) {
+            Items iFatCal = new Items("Calories from fat", UPCLookup.values.get("nf_calories_from_fat"));
+            Items iTotalFat = new Items("Total fat", UPCLookup.values.get("nf_total_fat"));
+            Items iSatFat = new Items("Saturated fat", UPCLookup.values.get("nf_saturated_fat"));
+            Items iTransFat = new Items("Trans fat", UPCLookup.values.get("nf_trans_fatty_acid"));
+            Items iPolyFat = new Items("Polyunsaturated fat", UPCLookup.values.get("nf_polyunsaturated_fat"));
+            Items iMonoFat = new Items("Monounsaturated fat", UPCLookup.values.get("nf_monounsaturated_fat"));
+            arrayOfItems.add(iFatCal);
+            arrayOfItems.add(iTotalFat);
+            arrayOfItems.add(iSatFat);
+            arrayOfItems.add(iTransFat);
+            arrayOfItems.add(iPolyFat);
+            arrayOfItems.add(iMonoFat);
+        }
+        if(MainActivity.cCholest.isChecked()) {
+            Items iCholest = new Items("Cholesterol", UPCLookup.values.get("nf_cholesterol"));
+            arrayOfItems.add(iCholest);
+        }
+        if(MainActivity.cSodium.isChecked()) {
+            Items iSodium = new Items("Sodium", UPCLookup.values.get("nf_sodium"));
+            arrayOfItems.add(iSodium);
+        }
+        if(MainActivity.cCarbs.isChecked()) {
+            Items iTotCarb = new Items("Total Carbs", UPCLookup.values.get("nf_total_carbohydrate"));
+            Items iSugar = new Items("Total sugars", UPCLookup.values.get("nf_sugars"));
+            arrayOfItems.add(iTotCarb);
+            arrayOfItems.add(iSugar);
+        }
+        if(MainActivity.cFiber.isChecked()) {
+            Items iFiber = new Items("Dietary Fiber", UPCLookup.values.get("nf_dietary_fiber"));
+            arrayOfItems.add(iFiber);
+        }
+        if(MainActivity.cProtein.isChecked()) {
+            Items iProtein= new Items("Total protein",UPCLookup.values.get("nf_protein"));
+            arrayOfItems.add(iProtein);
+        }
+        if(MainActivity.cVitA.isChecked()) {
+            Items iVitA= new Items("DV% vitamin A",UPCLookup.values.get("nf_vitamin_a_dv"));
+            arrayOfItems.add(iVitA);
+        }
+        if(MainActivity.cVitC.isChecked()) {
+            Items iVitC= new Items("DV% vitamin C",UPCLookup.values.get("nf_vitamin_c_dv"));
+            arrayOfItems.add(iVitC);
+        }
+        if(MainActivity.cCalcium.isChecked()) {
+            Items iCalcium = new Items("DV% calcium", UPCLookup.values.get("nf_calcium_dv"));
+            arrayOfItems.add(iCalcium);
+        }
+        if(MainActivity.cIron.isChecked()) {
+            Items iIron= new Items("DV% iron",UPCLookup.values.get("nf_iron_dv"));
+            arrayOfItems.add(iIron);
+        }
+
 
          // Create the itemAdapter to convert the array to views
          ItemAdapter itemAdapter = new ItemAdapter(this, arrayOfItems);
